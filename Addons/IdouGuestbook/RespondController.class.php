f67579f5ac73d3a1bd834b053d8cfc010f26fec3
phonen <pioul@tom.com>
2017-08-23 10:30:46 +0800

    1

---
+++ Addons/IdouGuestbook/RespondController.class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Addons\IdouGuestbook\Controller;
+use Mp\Controller\ApiController;
+
+/**
+ * 留言板响应控制器
+ * @author 么么哒
+ */
+class RespondController extends ApiController {
+
+	/**
+	 * 微信交互
+	 * @param $message array 微信消息数组
+	 */
+	public function wechat($message = array()) {
+					reply_text($message);
+	}
+}
+
+?>
