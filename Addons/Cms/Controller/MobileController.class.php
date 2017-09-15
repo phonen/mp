1c14dfadae26a24e1fd722abc9070d918dd3055f
phonen <pioul@tom.com>
2017-09-15 21:56:02 +0800

    1

--- Addons/Cms/Controller/MobileController.class.php
+++ Addons/Cms/Controller/MobileController.class.php
@@ -14,6 +14,7 @@ class MobileController extends MobileBaseController {
 
     public function _initialize()
     {
+        /*
         parent::_initialize();
 
         $setting=get_addon_settings();
@@ -28,7 +29,7 @@ class MobileController extends MobileBaseController {
 
             $this->lahei();//拉黑
         }
-
+*/
 
     }
     protected function login(){
